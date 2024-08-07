c4de5f8c5775d2acfd02a9e6e13270b0286eec7f
Nima77Moosavi <nima77moosavi@gmail.com>
2024-08-07 12:50:41 +0330

    created static and media root

--- website/views.py
+++ website/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 def index_view(request):
-    return HttpResponse("<h1>Index</h1>")
+    return render(request, "website/index.html")
 
 def about_view(request):
-    return HttpResponse("<h1>About</h1>")
+    return render(request, "website/about.html")
 
 def contact_view(request):
-    return HttpResponse("<h1>Contact</h1>")
+    return render(request, 'website/Contact.html')
