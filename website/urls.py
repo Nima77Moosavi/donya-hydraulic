964aede2ea1808ccaae46b65b6d8060cd1bbbd7a
Nima77Moosavi <nima77moosavi@gmail.com>
2024-08-15 12:42:40 +0330

    base frontend template done

--- website/urls.py
+++ website/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_view),
-    path('about/', views.about_view),
-    path('contact/', views.contact_view)
+    path('', views.index_view, name='index'),
+    path('about/', views.about_view, name='about'),
+    path('contact/', views.contact_view, name='contact')
 ]
